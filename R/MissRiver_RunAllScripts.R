d4f24396435b76e16711030a2f8e8b0552890e48
lukeloken <lukeloken@gmail.com>
2018-06-04 13:55:28 -0500

    code to run all UMR_MissRiver scripts

---
+++ R/MissRiver_RunAllScripts.R
@@ -0,0 +1,31 @@
+# Script to call other scripts and process UMR data
+rm(list = ls())
+
+setwd("E:/Git_Repo/MissRiver_Nitrogen")
+
+#Project Direcotries
+dir<-"E:/Git_Repo/MissRiver_Nitrogen"
+shapedir<-"E:/Dropbox/FLAME/basemaps/shapefiles"
+locdir<-"E:/Dropbox/ArcGIS"
+flamedir<-'E:/Dropbox/FLAME_MississippiRiver/Data/2015_UMR_AllDays'
+
+
+#Calculate UMR Pool areas and volume
+source('R/SummarizeUMRPoolAreas.R')
+
+# Using Dataretrieval download USGS gauge station discharge for UMR survey
+source('R/DownloadConcurrentDischarge.R')
+
+# merge tributary chemistry with flow
+source('R/CombineTribQandChemistry.R')
+
+# Combine USACE and USGS gauge data and select either for dam flow
+source('R/SelectUSGSorLDdischarge.R')
+
+# Loop through dams and calcuate retention for August 2015 UMR survey
+source('R/ModelNRetentionUMRPools.R')
+
+# Using Dataretrieval download USGS gauge station discharge for UMR survey
+source('R/PlotRetentionWithUncertainty.R')
+
+
